5e4162c1871fc2718cac1696514acee5215ea3de
Tatiana Karashtina <tkarashtina@mail.ru>
2024-11-28 21:37:30 +0300

    Исправила замечания в итоговом задании 8 спринта

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,27 +4,27 @@
 
 namespace transport::detail::geo {
 
-    struct Coordinates {
-        double lat;
-        double lng;
-        bool operator==(const Coordinates& other) const {
-            return lat == other.lat && lng == other.lng;
-        }
-        bool operator!=(const Coordinates& other) const {
-            return !(*this == other);
-        }
-    };
-
-    inline double ComputeDistance(Coordinates from, Coordinates to) {
-        using namespace std;
-        if (from == to) {
-            return 0;
-        }
-        static const double dr = 3.1415926535 / 180.;
-        static const double earth_radius = 6371000; // Радиус Земли
-        return acos(sin(from.lat * dr) * sin(to.lat * dr)
-            + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-            * earth_radius;
+struct Coordinates {
+    double lat;
+    double lng;
+    bool operator==(const Coordinates& other) const {
+        return lat == other.lat && lng == other.lng;
     }
+    bool operator!=(const Coordinates& other) const {
+        return !(*this == other);
+    }
+};
+
+inline double ComputeDistance(Coordinates from, Coordinates to) {
+    using namespace std;
+    if (from == to) {
+        return 0;
+    }
+    static const double dr = 3.1415926535 / 180.;
+    static const double earth_radius = 6371000; // Радиус Земли
+    return acos(sin(from.lat * dr) * sin(to.lat * dr)
+        + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
+        * earth_radius;
+}
 
 } // end namespace transport::detail::geo
